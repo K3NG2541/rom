c3dce81650de05c95e7b7c7687584e073f1af97d
K3NG2541 <jchan1531@gmail.com>
2017-11-25 13:27:12 +0000

    Edit AICP.sh

--- AICP.sh
+++ AICP.sh
@@ -10,11 +10,10 @@ echo "Preparing to sync"
 repo init -u https://github.com/AICP/platform_manifest.git -b n7.1
 repo sync -c -f --force-sync --no-clone-bundle --no-tags -j64
 echo "Sync complete Syncing device repos"
-git clone https://github.com/K3NG2541/android_device_lenovo_P1m -b aicp device/lenovo/P1m
-git clone https://github.com/K3NG2541/android_kernel_lenovo_P1m kernel/lenovo/P1m
-git clone https://github.com/K3NG2541/android_vendor_lenovo_P1m vendor/lenovo/P1m
+git clone https://github.com/K3NG2541/android_device_Lava_LAVA_R1 -b aicp device/Lava/LAVA_R1
+git clone https://github.com/K3NG2541/android_vendor_Lava_LAVA_R1 vendor/Lava/LAVA_R1
 echo "Apply Patches"
-cd device/lenovo/P1m/patches
+cd device/Lava/LAVA_R1/patches
 sudo bash apply-patches.sh
 echo "Fix Sepolicy"
 rm -r system/sepolicy
@@ -31,5 +30,5 @@ export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -X
 export USE_NINJA=false
 make clean && make clobber
 . build/envsetup.sh
-lunch aicp_P1m-userdebug
-brunch aicp_P1m-userdebug
+lunch aicp_LAVA_R1-userdebug
+brunch aicp_LAVA_R1-userdebug
